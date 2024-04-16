73c461431762179759996357d0c12c3c8f3a44f6
Cristopher <alum.2c@gmail.com>
2024-04-15 18:22:12 -0600

    6.9

--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,17 +2,18 @@ import { useSelector, useDispatch } from "react-redux"
 import {voteFor} from "../reducers/anecdoteReducer.js"
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state=>state)
+    const filter = useSelector(state => state.filter)
+    const anecdotes = useSelector(state => state.anecdotes)
     const dispatch = useDispatch()
+    const anecdotesToShow = anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter))
 
     const vote = (id) => {
       dispatch(voteFor(id))
     }
-
     return(
         <div>
             <h2>Anecdotes</h2>
-            {anecdotes.map(anecdote =>
+            {anecdotesToShow.map(anecdote =>
                 <div key={anecdote.id}>
                     <div>
                         {anecdote.content}

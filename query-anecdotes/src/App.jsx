6e5d17bf5fffbdac2eadbcbd7873ce60a1a2a05e
Cristopher <alum.2c@gmail.com>
2024-07-05 17:24:27 -0600

    exercises 6.20 6.21 6.22

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,19 +1,36 @@
+import { useQuery, useMutation, useQueryClient  } from '@tanstack/react-query'
+import { getAnecdotes,updateAnecdote  } from './request/requests'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+  const queryClient = useQueryClient()
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: () => getAnecdotes(),
+    retry: 1
+  })
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+  })
 
-  const handleVote = (anecdote) => {
-    console.log('vote')
+  const anecdotes = result.data
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  if ( result.isError ) {
+    return <span>anecdote service not available due to problems in server</span>
+  }
+
+  const handleVote = (anecdote) => {
+    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes+1 })
+  }
 
   return (
     <div>
--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,11 +1,23 @@
-const AnecdoteForm = () => {
+import { createAnecdote  } from '../request/requests'
+import { useMutation, useQueryClient  } from '@tanstack/react-query'
 
+const AnecdoteForm = () => {
+    const queryClient = useQueryClient()
+    const newAnecdoteMutation = useMutation({
+        mutationFn: createAnecdote,
+        onSuccess: (newAnecdote) => {
+            const anecdotes = queryClient.getQueryData(['anecdotes'])
+            queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+        },
+    })
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    if(content.length > 4){
+        newAnecdoteMutation.mutate({ content, votes: 0 })
+    }
+  }
 
   return (
     <div>
